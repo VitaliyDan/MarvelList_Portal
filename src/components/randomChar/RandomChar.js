a9ea02b3d6c4d05d5a42cbc321b3f9c6c2d9e193
VitaliyDanaykanich <vitailyc.ua@gmail.com>
2023-01-04 20:37:31 +0200

    update some functions ver 1.3

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,8 +1,9 @@
 import { Component } from 'react';
 import MarvelService from '../../services/MarvelService';
-import mjolnir from '../../resources/img/mjolnir.png'
 import './randomChar.scss';
-
+import Spinner from '../../resources/img/Spinner'
+import mjolnir from '../../resources/img/mjolnir.png'
+import ErrorMassage from '../errorMessage/ErrorMessage';
 
 class RandomChar extends Component {
 
@@ -12,40 +13,41 @@ class RandomChar extends Component {
     }
 
     state = {
-        char: {}
+        char: {},
+        loading: true,
+        error: false,
+
     }
     onCharLoaded=(char)=>{
-        this.setState({char});
+        this.setState({char, loading: false});
+    }
+
+    onError =()=>{
+        this.setState({
+            loading: false,
+            error: true,
+        })
     }
 
     marvelService = new MarvelService;
     randomChar=()=>{
 
         const id = Math.floor(Math.random()* (1011400-1011000) + 1011000);
-         this.marvelService.getCharacter(id).then(this.onCharLoaded)
+         this.marvelService.getCharacter(id)
+         .then(this.onCharLoaded)
+         .catch(this.onError);
     }
 
     render(){
-        const {char: {name, description, thumbnail, homepage, wiki}} = this.state;
+        const {char , loading, error} = this.state;
+        const errorMessage = error ? <ErrorMassage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? <Viev char ={char}/> : null;
         return (
             <div className="randomchar">
-                <div className="randomchar__block">
-                    <img src={thumbnail} alt="Random character" className="randomchar__img"/>
-                    <div className="randomchar__info">
-                        <p className="randomchar__name">{name}</p>
-                        <p className="randomchar__descr">
-                           {description ? description : 'We havent some information on this character'} 
-                        </p>
-                        <div className="randomchar__btns">
-                            <a href={homepage} className="button button__main">
-                                <div className="inner">Homepage</div>
-                            </a>
-                            <a href={wiki} className="button button__secondary">
-                                <div className="inner">Wiki</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                    {errorMessage}
+                    {spinner}
+                    {content}
                 <div className="randomchar__static">
                     <p className="randomchar__title">
                         Random character for today!<br/>
@@ -63,6 +65,29 @@ class RandomChar extends Component {
         )
     }
 
+}
+
+const Viev=({char}, )=>{
+        const {name, description, thumbnail, homepage, wiki} = char;
+    return(
+        <div className="randomchar__block">
+                    <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+                    <div className="randomchar__info">
+                        <p className="randomchar__name">{name}</p>
+                        <p className="randomchar__descr">
+                           {description ? description : 'We havent some information on this character'} 
+                        </p>
+                        <div className="randomchar__btns">
+                            <a href={homepage} className="button button__main">
+                                <div className="inner">Homepage</div>
+                            </a>
+                            <a href={wiki} className="button button__secondary">
+                                <div className="inner">Wiki</div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+    )
 }
 
 export default RandomChar;
